ff2cd31848be0799c59effa59ea9fa7fa339c84b
VW <34698193+QuietEvolver@users.noreply.github.com>
2022-11-04 16:08:31 -0700

    It will add sizing fields, radio for individual sizing, checkboxes for mult selects and css.

--- js/scripts.js
+++ js/scripts.js
@@ -77,6 +77,27 @@ function listCustomers(pizzaParlorDisplay) {
   });
 }
 
+const pizzaParlor = new PizzaParlor();
+
+
+window.addEventListener("load", function(){
+  const form = document.querySelector("form");
+const log = document.querySelector("#log");
+
+  form.addEventListener(
+    "submit",
+    (event) => {
+      const data = new FormData(form);
+      let output = "";
+      for (const entry of data) {
+        output = `${output}${entry[0]}=${entry[1]}\r`;
+      }
+      log.innerText = output;
+      event.preventDefault();
+    },
+    false
+  );
+});
 
 
 
