00cd3ede092a590e39a85964312e52768f3328dc
Vera Weikel <veraweikel@Veras-MacBook-Pro-3.local>
2022-11-11 07:31:39 -0800

    It will add sizing price.

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,16 @@
 function Order ( size, toppings ) {
   this.size = size;
   this.toppings = toppings;
+  this.price = 0;
 }
-order.size = "medium";
-order.toppings = ["anchovies", "pineapple"];
 
-// UI
-
+Order.prototype.calculateOrderPrice = function(){
+  if(this.size === "small"){
+    this.price = 5
+  } else if(this.size === "medium"){
+    this.price = 10
+  } else if(this.size === "large"){
+    this.price = 15
+    return this.price;
+  }
+}
