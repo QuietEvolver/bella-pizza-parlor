9b8cae5348f25bbbcd9b0d067ca72e24922fda1d
Vera Weikel <veraweikel@Veras-MacBook-Pro-3.local>
2022-11-12 10:43:38 -0800

    It will show attempt for checkbox values.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 // BI 
 function Order ( size, toppings ) {
   this.size = size;
-  this.toppings = toppings;
+  this.toppings = [];
   this.price = 0;
 }
 
-Order.prototype.calculateOrderPrice = function(){
+Order.prototype.calculateOrderPrice = function(e){
   if(this.size === "small"){
     this.price = 5 + (this.toppings.length);
   } else if(this.size === "medium"){
@@ -20,25 +20,31 @@ Order.prototype.calculateOrderPrice = function(){
 function handleFormSubmission(e){
   e.preventDefault();
   console.log("e: ", e);
-  // const size = document.querySelector("input[name='order-size']:radio");
+  const size = document.querySelector("input[name='order-size']:checked").value;
   // const toppings = document.querySelector('.topping').checked;
-  const orderToppings = document.querySelectorAll("input[name='toppings']:checked");
+  const toppings = document.querySelectorAll("input[name='toppings']:checked").value;
+  let order = new Order(size, toppings);
+  order.push(toppings);
   
-  const toppings = Array.from(orderToppings);
-  toppings.forEach(()=>{ // elem
-    const paragraph = document.createElement("p");
-    paragraph.append(document.getElementsByName("toppings.name"));
-    document.body.append(paragraph);
-  })
+  // const toppings = Array.from(orderToppings);
+  // toppings.forEach(()=>{ // elem
+  //   const paragraph = document.createElement("p");
+  //   paragraph.append(document.querySelectorAll(".toppings"));
+  //   document.body.append(paragraph);
+  // })
 
-  // toppings.forEach(function())
-  let order = new Order();
+  
   // order.size;
   order.toppings;
-  console.log("Toppings: ", toppings);
-  console.log("Order Toppings: ", orderToppings);
-  order.calculateOrderPrice(toppings);
-  console.log("Price: ", order.calculateOrderPrice(toppings));
+  console.log("Size: ", size);
+  order.calculateOrderPrice(size, toppings);
+  console.log("Price: ", order.calculateOrderPrice(size));
+  console.log("Toppings: ", order.toppings);
+  // console.log("Order Toppings: ", orderToppings);
+
+
+const output = document.querySelector(".price-output").innerText;
+
 
 }
 
