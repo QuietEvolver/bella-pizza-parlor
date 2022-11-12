ff4a1a8a2afc8683295a4626f55ee7b511e7802d
Vera Weikel <veraweikel@Veras-MacBook-Pro-3.local>
2022-11-12 07:53:51 -0800

    it will add js to pass test.

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,19 @@ function Order ( size, toppings ) {
   this.price = 0;
 }
 
-Order.prototype.calculateOrderPrice = function(){
+Order.prototype.calculateSizePrice = function(){
   if(this.size === "small"){
-    this.price = 5
+    this.price = 5;
   } else if(this.size === "medium"){
-    this.price = 10
+    this.price = 10;
   } else if(this.size === "large"){
-    this.price = 15
+    this.price = 15;
     return this.price;
   }
 }
+Order.prototype.calculateToppingsPrice = function(){
+  toppings.forEach(element => {
+    return element.length * 2;
+  });
+}
+// UI
