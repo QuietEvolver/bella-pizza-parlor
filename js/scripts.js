0c1d529ac3700f3e82076d2f9f1afa02f91fe2f7
Vera Weikel <veraweikel@Veras-MacBook-Pro-3.local>
2022-11-12 18:54:22 -0800

    It will capture toppings; price compromised.

--- js/scripts.js
+++ js/scripts.js
@@ -5,47 +5,58 @@ function Order ( size, toppings ) {
   this.price = 0;
 }
 
-Order.prototype.calculateOrderPrice = function(e){
+Order.prototype.calculateOrderPrice = function(){
   if(this.size === "small"){
-    this.price = 5 + (this.toppings.length);
+    this.price = 5 + (this.toppings.length *2);
   } else if(this.size === "medium"){
-    this.price = 10 + (this.toppings.length);
+    this.price = 10 + (this.toppings.length *2);
   } else if(this.size === "large"){
-    this.price = 15 + (this.toppings.length);
+    this.price = 15 + (this.toppings.length *2);
     return this.price;
   }
 }
 
 // UI
 function handleFormSubmission(e){
+
   e.preventDefault();
   console.log("e: ", e);
   const size = document.querySelector("input[name='order-size']:checked").value;
   // const toppings = document.querySelector('.topping').checked;
-  const orderToppings = document.querySelector(".toppings").value;
+  const orderToppings = document.querySelectorAll("input[name=toppings]:checked");
   console.log("Intake OrderToppings: ", orderToppings);
   // const toppings = Array.from(orderToppings);
   const toppings = [];
-  toppings.push(orderToppings);
-  let checkedOrderToppings = this.orderToppings;
-  // for(let i = 0; i<checkedOrderToppings.length; i++){ 
-  //   if(document.getElementById("pepperoni").checked===true){
-  //     toppings.push("pepperoni");
-  //   }
-  console.log("checked order toppings: ", 
-  checkedOrderToppings)
-  //   const paragraph = document.createElement("p");
-  //   paragraph.append(document.querySelectorAll(".toppings"));
-  //   document.body.append(paragraph);
- // };
-
+  // toppings.push(NodeList);
+  // console.log("toppings: ", toppings);
+  // console.log("toppings: ",  toppings.push(NodeList));
+  // let checkedOrderToppings = this.orderToppings;
+//   for(let i = 0; i<checkedOrderToppings.length; i++){ 
+    if(document.getElementById("pepperoni").checked===true){
+      toppings.push("pepperoni");
+    } 
+    if(document.getElementById("mushroom").checked===true){
+      toppings.push("mushroom");
+    }
+    if(document.getElementById("anchovies").checked===true){
+      toppings.push("anchovies");
+    } else {
+     window.alert("this is looping toppings");
+   }
+//   console.log("checked order toppings: ", 
+//   checkedOrderToppings)
+//   //   const paragraph = document.createElement("p");
+//   //   paragraph.append(document.querySelectorAll(".toppings"));
+//   //   document.body.append(paragraph);
+//  // };
+//   }
   
   let order = new Order(size, toppings);
   // order.size;
   order.toppings;
   console.log("Size: ", size);
-  order.calculateOrderPrice(size, toppings);
-  console.log("order.prototype.calcPrice: ", order.calculateOrderPrice(size, toppings));
+  order.calculateOrderPrice();
+  console.log("order.prototype.calcPrice: ", order.calculateOrderPrice(this.size, this.toppings));
   console.log("order.toppings: ", order.toppings);
   // console.log("Order Toppings: ", orderToppings);
 
