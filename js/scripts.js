5ce93caa17d02c3ba0db3e9beeb08baf7dea647d
Vera Weikel <veraweikel@Veras-MacBook-Pro-3.local>
2022-11-16 19:07:19 -0800

    It will attmept to capture order in new fx.

--- js/scripts.js
+++ js/scripts.js
@@ -1,54 +1,62 @@
 // BI 
-function Order ( size ) {//, toppings
+function Order ( size, toppings ) {
   this.size = size;
-  // this.toppings = toppings; 
-  // this.price = 0;
+  this.toppings = toppings; 
+  this.price = 0;
 }
 
-// Order.prototype.calculateOrderPrice = function(){
-//   if(this.toppings.price === 0){
-//     return this.size;
-//   }
-//   else if(this.size === "small"){
-//     this.price = 5 + (this.toppings.length *2);
-//     return this.price;
-//   } else if(this.size === "medium"){
-//     this.price = 10 + (this.toppings.length *2);
-//     return this.price;
-//   } else if(this.size === "large"){
-//     this.price = 15 + (this.toppings.length *2);
-//     return this.price;
-//   }
-// }
+Order.prototype.calculateOrderPrice = function(){
+  if(this.toppings.price === 0){
+    return this.size;
+  }
+  else if(this.size === "small"){
+    this.price = 5 + (this.toppings.length *2);
+    return this.price;
+  } else if(this.size === "medium"){
+    this.price = 10 + (this.toppings.length *2);
+    return this.price;
+  } else if(this.size === "large"){
+    this.price = 15 + (this.toppings.length *2);
+    return this.price;
+  }
+}
 
-// // UI
-// function handleFormSubmission(e){
+Order.prototype.selectedOrder = function(){
+  console.log("I am in the function.");
+  return "Your order price is  $" +  " for a " + this.size + " " + this.toppings + " pizza.  " + "Thanks for your patronage!"
+}
 
-//   e.preventDefault();
-//   console.log("e: ", e);
-//   const size = document.querySelector("input[name='order-size']:checked").value;
-//   console.log("size: ", size);
-//   const toppings = [];
-//   if(document.getElementById("pepperoni").checked===true){
-//     toppings.push("pepperoni");
-//   } 
-//   if(document.getElementById("mushroom").checked===true){
-//     toppings.push("mushroom");
-//   }
-//   if(document.getElementById("anchovies").checked===true){
-//     toppings.push("anchovies");
-//   } 
+// UI
+function handleFormSubmission(e){
+
+  e.preventDefault();
+  console.log("e: ", e);
+  const size = document.querySelector("input[name='order-size']:checked").value;
+  console.log("size: ", size);
+  const toppings = [];
+  console.log("toppings: ", toppings);
+
+  if(document.getElementById("pepperoni").checked===true){
+    toppings.push("pepperoni");
+  } 
+  if(document.getElementById("mushroom").checked===true){
+    toppings.push("mushroom");
+  }
+  if(document.getElementById("anchovies").checked===true){
+    toppings.push("anchovies");
+  } 
   
-//   let order = new Order(size, toppings);
-//   order.toppings;
-//   order.calculateOrderPrice();
+  let order = new Order(size, toppings);
+  order.toppings;
+  order.calculateOrderPrice();
 
-//   let output = order.calculateOrderPrice(this.size, this.toppings);
-//   document.getElementById("ouput", output);
-  
-//   document.querySelector('p').innerText = "Your order price is  $" + order.calculateOrderPrice(this.size, this.toppings) + " for a " + order.size + " " + order.toppings + " pizza.  " + "Thanks for your patronage!";
-// }
+  // let output = order.calculateOrderPrice(this.size, this.toppings);
+  // document.querySelector('p').innerText = "Your order price is  $" + order.calculateOrderPrice(this.size, this.toppings) + " for a " + order.size + " " + order.toppings + " pizza.  " + "Thanks for your patronage!";
+  let output = order.selectedOrder();
+  document.getElementById("ouput", output);
+  document.querySelector('p').innerText = `Your order is ${output.wholeOrder}`;
+}
 
-// window.addEventListener("load", ()=> {
-//   document.querySelector("form").addEventListener("submit", handleFormSubmission);
-// })
+window.addEventListener("load", ()=> {
+  document.querySelector("form").addEventListener("submit", handleFormSubmission);
+})
